dad220c261de548beb6e87ea2fa2a20760101414
Bruno <bruno6821@gmail.com>
2021-10-13 18:25:08 -0300

    REFACTOR DE BUGS PORRRAA !!!!

--- escola/serializer.py
+++ escola/serializer.py
@@ -1,4 +1,3 @@
-from django.db import models
 from rest_framework import serializers
 from escola.models import Aluno, Curso, Matricula
 
@@ -18,12 +17,24 @@ class CursoSerializer(serializers.ModelSerializer):
 class MatriculaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Matricula
-        fields = '__all__'
+        exclude = []
 
 
 class ListaMatriculasAlunoSerializer(serializers.ModelSerializer):
     curso = serializers.ReadOnlyField(source='curso.descricao')
     periodo = serializers.SerializerMethodField()
+
     class Meta:
         model = Matricula
         fields = ['curso', 'periodo']
+
+    def get_periodo(self, obj):
+        return obj.get_periodo_display()
+
+
+class ListaAlunosMatriculadosSerializer(serializers.ModelSerializer):
+    aluno_nome = serializers.ReadOnlyField(source='aluno.nome')
+
+    class Meta:
+        model = Matricula
+        fields = ['aluno_nome']
