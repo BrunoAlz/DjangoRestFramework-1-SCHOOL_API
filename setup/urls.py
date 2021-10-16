a830bfae9d815fab35dd6c3747439a6ec551e75d
Bruno Alves <Bruno6821@gmail.com>
2021-10-15 23:16:54 -0300

    Implementação de DOCUMENTAÇÃO Automática da API!

--- setup/urls.py
+++ setup/urls.py
@@ -4,8 +4,25 @@ from escola.views import AlunosViewSet, CursosViewSet, MatriculaViewSet, ListaMa
 from rest_framework import routers
 from django.conf.urls.static import static
 from django.conf import settings
-from django.utils.decorators import method_decorator
-from django.views.decorators.cache import cache_page
+#from django.utils.decorators import method_decorator
+#from django.views.decorators.cache import cache_page
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Api-Cursos",
+        default_version='v1',
+        description="Provedor de cadastro dos Usuários do sistema de cursos",
+        terms_of_service="#",
+        contact=openapi.Contact(email="Api@cursos.com.br"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 
 router = routers.DefaultRouter()
@@ -18,6 +35,7 @@ router.register('imagens', ImagemViewSet,  basename='Imagens')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('alunos/<int:pk>/matriculas/', ListaMatriculasAluno.as_view()),
-    path('cursos/<int:pk>/matriculas/', ListaAlunosMatriculados.as_view())
+    path('cursos/<int:pk>/matriculas/', ListaAlunosMatriculados.as_view()),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
